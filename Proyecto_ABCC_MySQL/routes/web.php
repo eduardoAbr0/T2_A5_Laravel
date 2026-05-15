b3995da09b76b6dad94786a1846c6474a3eee36a
ED A <eduardo.br@tecjerez.edu.mx>
2026-05-15 10:32:22 -0600

    Rutas para login/logout

--- Proyecto_ABCC_MySQL/routes/web.php
+++ Proyecto_ABCC_MySQL/routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\CustomAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('alumnos.index');
 });
 
 Route::resource('alumnos',
     AlumnoController::class
 );
+
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::post('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('alumnos', AlumnoController::class);
+});
