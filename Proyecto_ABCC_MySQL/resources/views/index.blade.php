316e6b40ae487553fea4491c7c46a4f522971f52
ED A <eduardo.br@tecjerez.edu.mx>
2026-05-15 12:28:11 -0600

    CDN de bootstrap para index

--- Proyecto_ABCC_MySQL/resources/views/index.blade.php
+++ Proyecto_ABCC_MySQL/resources/views/index.blade.php
@@ -103,6 +103,9 @@
 @section('js')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+  
 <link rel="stylesheet" href="https://cdn.datatables.net/2.3.8/css/dataTables.dataTables.css" />
 <script src="https://cdn.datatables.net/2.3.8/js/dataTables.js"></script>
 
