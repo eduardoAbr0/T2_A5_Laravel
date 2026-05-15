06035cb15d9618411f94087e636b7e761d235ab6
ED A <eduardo.br@tecjerez.edu.mx>
2026-05-15 13:03:12 -0600

    Archivos para deploy

--- Proyecto_ABCC_MySQL/resources/views/index.blade.php
+++ Proyecto_ABCC_MySQL/resources/views/index.blade.php
@@ -3,8 +3,6 @@
 @section('content')
 <div class="col-md-10">
 
-  <!-- NVEGACION -->
-
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item" aria-current="page">Inicio</li>
@@ -13,21 +11,13 @@
   </nav>
 
   <div class="row">
-
     <div class="col-md-12">
-
       <div class="content-box-large">
-        @if (session('success'))
-          <h1>
-            {{ session('success') }}
-          </h1>
-        @endif  
 
         <div class="panel-heading">
           <div class="panel-title">
             <h2>Listado de Alumnos</h2>
           </div>
-
         </div>
 
         <div class="panel-body">
@@ -39,10 +29,10 @@
           @endif
 
           <a href="{{route('alumnos.create')}}" class="btn btn-success mt-4 ml-3"> AGREGAR </a>
-          <section class="example mt-4">
-            <div class="table-responsive" id="tablaAlumnos">
 
-              <table class='table table-striped table-bordered table-hover text-center' id="tabla_alumnos">
+          <section class="example mt-4">
+            <div class="table-responsive">
+              <table class="table table-striped table-bordered table-hover text-center" id="tabla_alumnos">
                 <thead>
                   <tr>
                     <th>Numero de Control</th>
@@ -55,73 +45,51 @@
                 <tbody>
                   @foreach ($alumnos as $a)
                     <tr>
-                      <td class=v-align-middle>{{$a->Num_Control}}</td>
-                      <td class=v-align-middle>{{$a->Nombre}}</td>
-                      <td class=v-align-middle>{{$a->Semestre}}</td>
-                      <td class=v-align-middle>{{$a->Fecha_Nac}}</td>
-                      <td class=v-align-middle>
-
-                        <form action="{{ route('alumnos.destroy', $a) }}" method="POST" class="form-horizontal"
-                          role="form" onsubmit="return confirmarEliminacion()">
+                      <td>{{$a->Num_Control}}</td>
+                      <td>{{$a->Nombre}}</td>
+                      <td>{{$a->Semestre}}</td>
+                      <td>{{$a->Fecha_Nac}}</td>
+                      <td>
+                        <form action="{{ route('alumnos.destroy', $a) }}" method="POST" onsubmit="return confirmarEliminacion()">
                           @csrf
                           @method('DELETE')
-
-                          <a class="btn btn-primary" href="{{route('alumnos.show', $a->id)}}">Detalle</a>
-                          <a class="btn btn-warning" href="{{route('alumnos.edit', $a->id) }}">Editar</a>
-
-                          <button type="submit" class="btn btn-danger">
-                            ELIMINAR </button>
-
+                          <a class="btn btn-primary btn-sm" href="{{route('alumnos.show', $a->id)}}">Detalle</a>
+                          <a class="btn btn-warning btn-sm" href="{{route('alumnos.edit', $a->id)}}">Editar</a>
+                          <button type="submit" class="btn btn-danger btn-sm">ELIMINAR</button>
                         </form>
                       </td>
                     </tr>
                   @endforeach
                 </tbody>
               </table>
-              {{-- {!! $alumnos->links() !!}--}}
-
             </div>
           </section>
 
         </div>
-
       </div>
-
     </div>
-
   </div>
-
 </div>
 @stop
 
 @section('css')
-{{-- Add here extra stylesheets --}}
-{{--
-<link rel="stylesheet" href="/css/admin_custom.css"> --}}
+<link rel="stylesheet" href="https://cdn.datatables.net/2.3.8/css/dataTables.dataTables.css" />
 @stop
 
 @section('js')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script type="text/javascript" src="./js/bootstrap.min.js"></script>
-  
-<link rel="stylesheet" href="https://cdn.datatables.net/2.3.8/css/dataTables.dataTables.css" />
 <script src="https://cdn.datatables.net/2.3.8/js/dataTables.js"></script>
 
-<!-- JQUERY para desaparecer mensajes-->
-<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-<script type="text/javascript">
+<script>
   $(document).ready(function () {
     setTimeout(function () {
       $("#msj").fadeOut(1500);
     }, 3000);
+
+    $('#tabla_alumnos').DataTable();
   });
-</script>
 
-<script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-
-<script>
-  let table = new DataTable('#tabla_alumnos');
+  function confirmarEliminacion() {
+    return confirm('¿Estás seguro de que deseas eliminar este alumno?');
+  }
 </script>
 @stop
